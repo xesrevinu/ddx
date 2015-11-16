48cc04b3d53faa935c2304d4656134ae1e6fac28
xiaokekeT <121441909@qq.com>
2015-11-16 19:33:42 +0800

    fix 线上api地址

--- app/src/actions/apis.js
+++ app/src/actions/apis.js
@@ -2,8 +2,7 @@
  * Created by kee on 15/10/21.
  */
 import axios from 'axios';
-// const host = __DEVELOPMENT__ ? '//localhost:3000/api' : '/api';
-const host = '//localhost:3000/api';
+const host = __DEVELOPMENT__ ? '//localhost:3000/api' : '/api';
 
 export default function apis({ url, method, ...others }) {
   return axios({
